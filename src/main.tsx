2ae0f919279442007d413063310c7cf88b1fd9cf
Imam Cahyadi <imamtjahjadi@gmail.com>
2026-09-01 21:19:55 +0700

    style: refresh landing page footer branding

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Home from '../app/page';
 import '../app/globals.css';
+import '../app/footer.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><Home /></StrictMode>);
